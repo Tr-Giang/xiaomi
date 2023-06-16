bf045b61f88d8372e20955fdc6ff45d3b6fb97b3
Nguyễn Trường Giang <nguyentruonggiang190309@gmail.com>
2023-06-16 09:45:58 +0700

    Update: upload home page xiaomi.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # xiaomi
+
+1/6 : fix lại bug hover phần header + đang responsive phần header.
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,59 @@
-const app = {
-    handleOverlay() {
-        const overlayNode = document.querySelector('.overlay');
-        const subnavBoxs = document.querySelectorAll('.header__navbar__item:not(.header__other>.header__navbar__item:first-child)');
-        subnavBoxs.forEach((subnavBox) => {
-            subnavBox.onmouseover = () => {
-                overlayNode.classList.add('overlay--open');
-            };
-            subnavBox.onmouseout = () => {
-                overlayNode.classList.remove('overlay--open');
-            };
-        });
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
-    },
+const app = (() => {
+    const overlayNode = $('.overlay');
+    const subnavBoxs = $$('.header__navbar__item:not(.header__other>.header__navbar__item:first-child)');
+    const headerBox = $('.header__box');
+    const headerBtn = $('.header__btn__nav');
+    const headerOpenSubs = [...$$('.header__navbar:not(.header__navbar.header__other) .header__navbar__item'), ...$$('.header__respon .header__respon__item')];
 
-    start() {
-        this.handleOverlay();
-    }
-};
+    return {
+        handleOverlay() {
+            subnavBoxs.forEach((subnavBox) => {
+                subnavBox.onmouseover = () => {
+                    overlayNode.classList.add('overlay--open');
+                };
+                subnavBox.onmouseout = () => {
+                    overlayNode.classList.remove('overlay--open');
+                };
+            });
+
+        },
+
+        handleClick() {
+            headerBtn.onclick = () => {
+                headerBtn.querySelector('.fa-bars').classList.toggle('display-none');
+                headerBtn.querySelector('.fa-xmark').classList.toggle('display-none');
+                headerBox.classList.toggle('show');
+
+                if (headerBtn.querySelector('.fa-bars').classList.contains('display-none')) headerBtn.style.right = '0';
+                else headerBtn.style.right = '2rem';
+            };
+
+            headerOpenSubs.forEach(headerOpenSub => {
+                headerOpenSub.firstElementChild.onclick = (e) => {
+                    e.preventDefault();
+                    headerOpenSub.querySelector('div .fa-plus').classList.toggle('display-none');
+                    headerOpenSub.querySelector('div .fa-minus').classList.toggle('display-none');
+
+
+                    if (headerOpenSub.querySelector('div .fa-plus').classList.contains('display-none')) { headerOpenSub.lastElementChild.style.height = `${headerOpenSub.firstElementChild.nextElementSibling.firstElementChild.clientHeight}px`; }
+                    else { headerOpenSub.lastElementChild.style.height = '0'; }
+
+                    console.log(headerOpenSub.firstElementChild.nextElementSibling.firstElementChild.clientHeight);
+                    console.log(headerOpenSub.firstElementChild.nextElementSibling.firstElementChild);
+                };
+
+            });
+
+        },
+
+        start() {
+            this.handleOverlay();
+            this.handleClick();
+        }
+    };
+})();
 
 app.start();
